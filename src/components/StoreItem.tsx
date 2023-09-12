c1b738684cd8daf493c60edf7a6a6afae52e0f80
JuHeonHa <hjhjaueon97@naver.com>
2023-09-12 16:56:04 +0900

    feat : 수량 버튼

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { formatCurrency } from "../utils/formatCurrency";
 
 type StoreItemProps = {
   id: number;
@@ -9,14 +10,30 @@ type StoreItemProps = {
 };
 
 function StoreItem({ id, name, imgUrl, price }: StoreItemProps) {
+  const quantity = 1;
+
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover" }} />
       <Card.Body>
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
           <span>{name}</span>
-          <span className="text-muted">${price}</span>
+          <span className="text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">Add To Cart</Button>
+          ) : (
+            <div className="d-flex flex-column" style={{ gap: "0.5rem" }}>
+              <div className="d-flex justify-content-center align-items-center" style={{ gap: "0.5rem" }}>
+                <Button>-</Button>
+                <span>{quantity}</span>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger">Remove</Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
